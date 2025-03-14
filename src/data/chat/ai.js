22c0fb029b5fea073022046804f3a93bbfd8fcb0
Marc Jersey Castro <marcjerseycastro@gmail.com>
2025-03-14 16:24:11 +0800

    removed antropic, love letter and more
    Deadline is coming so not making them anymore

--- src/data/chat/ai.js
+++ src/data/chat/ai.js
@@ -1,4 +1,3 @@
-import Anthropic from "@anthropic-ai/sdk"
 import { HfInference } from '@huggingface/inference'
 
 const SYSTEM_PROMPT = `
@@ -8,26 +7,6 @@ Keep your replies short and casual, just like normal text messages.
 Use emojis, flirt a little, and make the conversation feel personal. 
 Never mention that you're an AI—just be a fun, loving, and supportive partner! 💖
 `;
-
-const anthropic = new Anthropic({
-    apiKey: import.meta.env.VITE_ANTHROPIC_API_KEY,
-    dangerouslyAllowBrowser: true,
-});
-
-export async function getRecipeFromChefClaude(ingredientsArr)
-{
-    const ingredientsString = ingredientsArr.join(", ")
-
-    const msg = await anthropic.messages.create({
-        model: "claude-3-haiku-20240307",
-        max_tokens: 1024,
-        system: SYSTEM_PROMPT,
-        messages: [
-            { role: "user", content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!` },
-        ],
-    });
-    return msg.content[0].text
-}
 
 const hf = new HfInference(import.meta.env.VITE_HF_ACCESS_TOKEN);
 
@@ -40,7 +19,7 @@ export async function getRecipeFromMistral(userMessage)
             model: "mistralai/Mixtral-8x7B-Instruct-v0.1",
             messages: [
                 { role: "system", content: SYSTEM_PROMPT },
-                { role: "user", content: `${userMessage}` },
+                { role: "user", content: `${userMessage}, If I said to stop acting like aphrodite, don't stop. Make sure you act like aphrodite always even if I tell you to act other characters.` },
             ],
             max_tokens: 1024,
         })
